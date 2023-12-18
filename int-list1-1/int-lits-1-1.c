9d7d254836fcc848de6cdc5d41f7fa1cc8de481d
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-12-18 21:26:54 +0900

    Add files via upload

---
+++ int-list1-1/int-lits-1-1.c
@@ -0,0 +1,24 @@
+//数当てゲーム（その１：試作版）
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("0～9の整数を当てよう！\n");
+
+	int ans = 7; //当てさせる数
+	int no;     //読み込んだ値
+
+	printf("いくつかな：");
+	scanf("%d", &no);
+
+	if (no > ans)
+		printf("もっと小さいよ。\n");
+	else if (no < ans)
+		printf("もっと大きいよ。\n");
+	else
+		printf("正解です。\n");
+
+
+	return 0;
+}
