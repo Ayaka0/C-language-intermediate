e130e5f388c35a6029042b0d2c15933459f7a775
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-12-18 21:40:25 +0900

    Add files via upload

---
+++ int-list-1-3/int-list-1-3.c
@@ -0,0 +1,27 @@
+//数当てゲーム（その２：当たるまで繰り返す：while文を利用）
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("0～9の整数を当てよう\n");
+
+	int ans = 7;   //当てさせる数
+	int no;       //読み込んだ値
+
+	while (1) {
+		printf("いくつかな：");
+		scanf("%d", &no);
+
+		if (no > ans)
+			printf("もっと小さいよ。\n");
+		else if (no < ans)
+			printf("もっと大きいよ。\n");
+		else
+			break;
+	}
+
+	printf("正解です。\n");
+
+	return 0;
+}
