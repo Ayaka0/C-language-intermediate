9321177a6d72ff0511427d1348916119e2a192b7
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2024-01-17 04:49:30 +0900

    Add files via upload

---
+++ int-list6-4/int-list6-4.c
@@ -0,0 +1,22 @@
+#include<time.h>
+#include<stdio.h>
+
+//—v‘f•Ê‚Ì‚ğasctimeŠÖ”‚É€‚¶‚ÄŒğŠ·i‰üs•¶š‚ğ•t‰Á‚µ‚È‚¢j
+char* asctime(const struct tm* timeptr)
+{
+	const char wday_name[7][3] = {
+		"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+	};
+	const char mon_name[12][3] = {
+		"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+	};
+	static char result[25];	//•¶š—ñŠi”[æ‚ÍÃ“I‚È—Ìˆæ
+
+	sprintf(result, "%.3s %.3s %02d %02d:%02d:%02d %4d",
+		wday_name[timeptr->tm_wday], mon_name[timeptr->tm_mon],
+		timeptr->tm_mday, timeptr->tm_hour, timeptr->tm_min,
+		timeptr->tm_sec, timeptr->tm_year + 1900);
+
+	return result;
+
+}
