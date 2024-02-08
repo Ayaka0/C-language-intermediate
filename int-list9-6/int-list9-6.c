f7575dcf9e4ef10a684910202e29eb160ead0f26
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2024-02-09 04:49:54 +0900

    Add files via upload

---
+++ int-list9-6/int-list9-6.c
@@ -0,0 +1,55 @@
+//hdump…ファイルへのダンプ
+
+#include<ctype.h>
+#include<stdio.h>
+#include<limits.h>
+
+//ストリームsrcの内容をdstへダンプ
+void hdump(FILE* src, FILE* dst)
+{
+	int n;
+	unsigned long count = 0;
+	unsigned char buf[16];
+
+	while ((n = fread(buf, 1, 16, src)) > 0) {
+		fprintf(dst, "%08lX", count);	//アドレス
+
+		for (int i = 0; i < n; i++)	//16進数
+			fprintf(dst, "%0*X ", (CHAR_BIT + 3) / 4, (unsigned)buf[i]);
+
+		if (n < 16)
+			for (int i = n; i < 16; i++)
+				fputs("  ", dst);
+
+		for (int i = 0; i < n; i++)	//文字
+			fputc(isprint(buf[i]) ? buf[i] : '.', dst);
+		fputc('\n', dst);
+
+		count += 16;
+	}
+	fputc('\n', dst);
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+		hdump(stdin, stdout);	//標準入力→標準出力
+	else {
+		FILE* fp;
+
+		while (--argc > 0) {
+			if ((fp = fopen(*++argv, "rb")) == NULL) {
+				fprintf(stderr, "ファイル%sが正しくオープンできません。\n", *argv);
+				return 1;
+			}
+			else {
+				hdump(fp, stdout);		//ストリームfp→標準出力
+				fclose(fp);
+			}
+		}
+	}
+
+
+
+	return 0;
+}
